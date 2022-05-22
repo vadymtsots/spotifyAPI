5e7d86c017dba27f16d7f4c1a85c68a68ac3f77f
Vadim Matsotskyi <vadymtsots@gmail.com>
2022-05-22 14:51:43 +0300

    - refactoring

--- app/Mappers/Track/Track.php
+++ app/Mappers/Track/Track.php
@@ -4,88 +4,37 @@ namespace App\Mappers\Track;
 
 class Track
 {
-    /**
-     * @var object
-     */
     public object $album;
 
-    /**
-     * @var array
-     */
     public array $artists;
 
-    /**
-     * @var int
-     */
     public int $disk_number;
 
-    /**
-     * @var int
-     */
     public int $duration_ms;
 
-    /**
-     * @var bool
-     */
     public bool $explicit;
 
-    /**
-     * @var object
-     */
     public object $external_ids;
 
-    /**
-     * @var object
-     */
     public object $external_urls;
 
-    /**
-     * @var string
-     */
     public string $href;
 
-    /**
-     * @var string
-     */
     public string $id;
 
-    /**
-     * @var bool
-     */
     public bool $is_local;
 
-    /**
-     * @var bool
-     */
     public bool $is_playable;
 
-    /**
-     * @var string
-     */
     public string $name;
 
-    /**
-     * @var int
-     */
     public int $popularity;
 
-    /**
-     * @var string|null
-     */
-    public ?string $preview_url;
+    public string|null $preview_url;
 
-    /**
-     * @var int
-     */
     public int $track_number;
 
-    /**
-     * @var string
-     */
     public string $type;
 
-    /**
-     * @var string
-     */
     public string $uri;
 }
