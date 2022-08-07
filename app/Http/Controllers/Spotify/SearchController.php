578c6393941055cab6e45f86ed280a6b29a356be
Vadim Matsotskyi <vadymtsots@gmail.com>
2022-08-07 13:32:16 +0300

    Swagger integration:
    - imported l5-swagger lib;
    - added docs for Spotify search controller

--- app/Http/Controllers/Spotify/SearchController.php
+++ app/Http/Controllers/Spotify/SearchController.php
@@ -11,6 +11,35 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * @OA\Post(
+ *     path="/api/spotify/search",
+ *     description="Search for artist, album or track in Spotify API",
+ *     tags={"Search"},
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/x-www-form-urlencoded",
+ *             @OA\Schema(
+ *                 @OA\Property(property="search", description="Search query", example="Sepultura"),
+ *                 @OA\Property(property="type", description="Type of search: artist, album or track",
+ *                  example="album, artist, track"),
+ *                 required={"search"}
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful request",
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="Validation exception, if search query is empty",
+ *     ),
+ * )
+ *
+ */
+
 class SearchController extends SpotifyController
 {
     public function __invoke(Request $request, SearchProcessor $processor): JsonResponse
